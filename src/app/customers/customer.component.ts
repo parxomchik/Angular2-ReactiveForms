9836aadcb8de906a3dfa55b364f96c1364d85f2b
Dmitriy Parkhomenko <parkhomenko@MacBook-Pro-Dmitriy.local>
2017-06-04 20:28:33 +0300

    added reactive form module and refacrored customer form

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
 import { Customer } from './customer';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
+export class CustomerComponent implements OnInit {
+    customerForm: FormGroup;
     customer: Customer= new Customer();
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    ngOnInit(): void {
+        this.customerForm = new FormGroup({
+            firstName: new FormControl(),
+            lastName: new FormControl(),
+            email: new FormControl(),
+            sendCatalog: new FormControl(true)
+        });
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     }
  }
