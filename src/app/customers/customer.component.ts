1e28d532925b5c3b283f34f6c2a2445b7d8af603
Dmitriy Parkhomenko <parkhomenko@MacBook-Pro-Dmitriy.local>
2017-06-04 22:48:55 +0300

    added validation rule for phone number

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from './customer';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'my-signup',
@@ -16,9 +16,11 @@ export class CustomerComponent implements OnInit {
 
     ngOnInit(): void {
         this.customerForm = this.fb.group({
-            firstName: '',
-            lastName: '',
-            email: '',
+            firstName: ['', [Validators.required, Validators.minLength(3)] ],
+            lastName: ['', [Validators.required, Validators.maxLength(50)] ],
+            email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+            phone: '',
+            notification: 'email',
             sendCatalog: true
         });
     }
@@ -26,5 +28,12 @@ export class CustomerComponent implements OnInit {
     save() {
         console.log(this.customerForm);
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
+    setNotification(notifyVia: string): void {
+        const phoneControl = this.customerForm.get('phone');
+        notifyVia === 'text' ? phoneControl.setValidators(Validators.required) : phoneControl.clearValidators();
+        phoneControl.updateValueAndValidity();
+
     }
  }
